8d1e5eb08a2cdc83745641198e7363aba2c8782a
rk <errnivonrafal@gmail.com>
2019-01-27 01:21:13 +0100

    Add suspend key shortcut.

--- config.def.h
+++ config.def.h
@@ -65,6 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *suspcmd[]  = { "systemctl", "suspend", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -101,6 +102,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = suspcmd } },
 };
 
 /* button definitions */
